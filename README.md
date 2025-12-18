81c292d5afcb5c5ba5ba6af719a1a4f51b0d47ca
TRWither <iamwither5666@outlook.com>
2025-12-18 08:57:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shard for VSCode
+
+*Shard for VSCode* is a **VSCodium/VSCode** extension that supports **the Shard programming language**.
---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Shard for VSCode is active!');
+}
+
+export function deactivate() {}
